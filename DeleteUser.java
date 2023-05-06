f6c5725f9509c65e27ffa556b76a13509a177e1c
Haloisawesome <71446134+Haloisawesome@users.noreply.github.com>
2023-05-05 20:13:36 -0500

    Add files via upload

---
+++ DeleteUser.java
@@ -0,0 +1,65 @@
+package org.exploremore;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteUser implements ActionListener {
+    JFrame frame = new JFrame();
+    JButton deleteButton = new JButton("DELETE");
+    JTextField emailField = new JTextField();
+    JLabel emailLabel = new JLabel("Email:");
+    JLabel messageLabel = new JLabel();
+
+    DeleteUser() {
+        emailLabel.setBounds(50, 50, 150, 25);
+        messageLabel.setBounds(125, 150, 250, 35);
+        messageLabel.setFont(new Font(null, Font.ITALIC, 25));
+        emailField.setBounds(200, 50, 200, 25);
+        deleteButton.setBounds(125, 100, 200, 25);
+        deleteButton.setFocusable(false);
+        deleteButton.addActionListener(this);
+        frame.add(emailLabel);
+        frame.add(messageLabel);
+        frame.add(emailField);
+        frame.add(deleteButton);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(420, 250);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == deleteButton) {
+            String email = emailField.getText().trim();
+
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/jdbcdemo", "explorer", "rhett"
+                );
+                String query = "DELETE FROM user WHERE email = ?";
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setString(1, email);
+                int rowsAffected = statement.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    messageLabel.setForeground(Color.green);
+                    messageLabel.setText("User Deleted");
+                } else {
+                    messageLabel.setForeground(Color.red);
+                    messageLabel.setText("Failed");
+                }
+
+                connection.close();
+            } catch (ClassNotFoundException | SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+}
